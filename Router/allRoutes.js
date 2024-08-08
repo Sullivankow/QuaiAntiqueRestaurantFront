7a134d6faebd621a54c25dd9f86046996551f6c9
SundlyDev <sundly@live.fr>
2024-08-08 10:25:44 +0200

    install Bootstrap

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,9 +1,8 @@
 import Route from "./Route.js";
 
-//Définir ici vos routes 
+//Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Acceuil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),];
 
-    //Le titre s'affiche comme ceci: Route.titre - websitename
-
-    export const websitename = "Quai Antique";
+//Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Quai Antique";
--- Router/router.js
+++ Router/router.js
@@ -1,69 +1,65 @@
 import Route from "./Route.js";
-import {allRoutes, websitename} from "./allRoutes.js";
+import { allRoutes, websiteName } from "./allRoutes.js";
 
-//Création d'une route pour la page 404 (page introuvable)
+// Création d'une route pour la page 404 (page introuvable)
 const route404 = new Route("404", "Page introuvable", "/pages/404.html");
 
-//Foncction pour récupérer la route correspondant à une URL donnée
+// Fonction pour récupérer la route correspondant à une URL donnée
 const getRouteByUrl = (url) => {
-    let currentRoute = null;
-
-    //Parcours de toutes les routes pour trouver la correspondance
-    allRoutes.forEach((element) => {
-        if (element.url == url) {
-            currentRoute = element;
-
-        }
-    });
-    
-    //Si aucue correspndance n'est trouvée, on retourne la route 404
-    if (currentRoute != null) {
-        return currentRoute;
-
-    }else{
-        return route404;
+  let currentRoute = null;
+  // Parcours de toutes les routes pour trouver la correspondance
+  allRoutes.forEach((element) => {
+    if (element.url == url) {
+      currentRoute = element;
     }
+  });
+  // Si aucune correspondance n'est trouvée, on retourne la route 404
+  if (currentRoute != null) {
+    return currentRoute;
+  } else {
+    return route404;
+  }
 };
 
-//Fonction pour charger le contenu de la page
+// Fonction pour charger le contenu de la page
 const LoadContentPage = async () => {
-    const path = window.location.pathname;
+  const path = window.location.pathname;
+  // Récupération de l'URL actuelle
+  const actualRoute = getRouteByUrl(path);
+  // Récupération du contenu HTML de la route
+  const html = await fetch(actualRoute.pathHtml).then((data) => data.text());
+  // Ajout du contenu HTML à l'élément avec l'ID "main-page"
+  document.getElementById("main-page").innerHTML = html;
 
-    //Récupération de l'URL actuelle
-    const actualRoute = getRouteByUrl(path);
+  // Ajout du contenu JavaScript
+  if (actualRoute.pathJS != "") {
+    // Création d'une balise script
+    var scriptTag = document.createElement("script");
+    scriptTag.setAttribute("type", "text/javascript");
+    scriptTag.setAttribute("src", actualRoute.pathJS);
 
-    //Récupération du contenu HTML de la route
-    const html = await fetch(actualRoute.pathHtml).then((data) => data.text());
-        //Ajout du contenu HTML à l'élément avec l'id "main-page"
-    document.getElementById("main-page").innerHTML = html;
+    // Ajout de la balise script au corps du document
+    document.querySelector("body").appendChild(scriptTag);
+  }
 
-    //Ajout du contenu Javascript
-    if (actualRoute.pathJS !="") {
-        //Création d'une balise script
-        var scriptTag = document.createElement("script");
-        scriptTag.setAttribute("type", "text/javascript");
-        scriptTag.setAttribute("src", actualRoute.pathJS);
-
-        //Ajout de la balise script au corps du document
-        document.querySelector("body").appendChild(scriptTag);
-    }
-//Changement du titre de la page
-document.title = actualRoute.title + " - " + websitename;
+  // Changement du titre de la page
+  document.title = actualRoute.title + " - " + websiteName;
 };
 
-
-//Fonction pour gérer les évènements de routage (clic sur les liens)
+// Fonction pour gérer les événements de routage (clic sur les liens)
 const routeEvent = (event) => {
-    event = event || window.event;
-    event.preventDefault();
-
-    //Mise à jour de l'URL dans l'historique du navigateur 
-    window.history.pushState({}, "", event.target.href);
-
-    //Chargement du contenu de la nouvelle page
-    LoadContentPage();
+  event = event || window.event;
+  event.preventDefault();
+  // Mise à jour de l'URL dans l'historique du navigateur
+  window.history.pushState({}, "", event.target.href);
+  // Chargement du contenu de la nouvelle page
+  LoadContentPage();
 };
 
-//Gestion de l'évènement de retour en arrière dans l'historique du navigateur 
+// Gestion de l'événement de retour en arrière dans l'historique du navigateur
 window.onpopstate = LoadContentPage;
+// Assignation de la fonction routeEvent à la propriété route de la fenêtre
+window.route = routeEvent;
+// Chargement du contenu de la page au chargement initial
+LoadContentPage();
 
