c3d7f85d06dc3eae2113645ade5fc191e51682f4
SundlyDev <sundly@live.fr>
2024-08-15 19:53:56 +0200

    Reservations finished

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -10,6 +10,7 @@ export const allRoutes = [
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changement de mot de passe", "/pages/auh/editPassword.html"),
     new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
+    new Route("/reserver", "reserver", "/pages/reservations/reserver.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
