33bd51badf74633c1b09acb1c77780a82cc00194
SundlyDev <sundly@live.fr>
2024-08-23 15:05:00 +0200

    add authentifier une rêquete avec fonction getInfosUser

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ const apiUrl = "https://127.0.0.1:8000/api/"
 
 signoutBtn.addEventListener("click", signout);
 
+
 function signout(){
     eraseCookie(tokenCookieName);
     eraseCookie(RoleCookieName)
@@ -117,3 +118,38 @@ function sanitizeHtml(text){
     
     }
 
+
+    //Méthode permettant d'authentifier une requête via JS
+
+    //Récupérer les infos de l'utilisateur
+    function getInfosUser(){
+    
+
+        
+    let myHeaders = new Headers();
+    myHeaders.append("X-AUTH-TOKEN", getToken());
+
+    let requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+      };
+
+      fetch(apiUrl +"account/me", requestOptions)
+      .then(response => {
+        if(response.ok){
+            return response.json();
+        } 
+        else {
+            console.log("Impossible de récupérer les informations utilisateur");
+        }
+
+      })
+
+      .then(result => {
+        return(result);
+      })
+
+      .catch(error =>{
+        console.log("Erreur lors de la récupération de données utilisateur", error);
+      })};
